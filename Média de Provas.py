afb8d28e54d4257d84f0498888bf308135a412f1
Lucas Santana <47119652+santanaluc@users.noreply.github.com>
2019-02-11 20:44:06 -0200

    First class of the python in the college
    first class exercise

---
+++ Média de Provas.py
@@ -0,0 +1,43 @@
+
+# coding: utf-8
+
+# In[3]:
+
+
+n1=input('QUAL É O SEU NOME? ')
+print('prazer, {}'.format(n1))
+
+
+# In[ ]:
+
+
+p1 = float(input('qual é a sua nota p1? '))
+p2 = float(input('qual sua nota p2? '))
+media = (p1+p2)/2
+print('com as notas da p1 {} e da p2 {}\n a média é igual a {}'.format(p1, p2, media))
+if(media>=6):
+      print('PARABÉNS VOCE FOI MUITO BEM')
+else:
+      print('tu é um lixo mesmo')
+
+
+# In[23]:
+
+
+p1 = float(input('qual é a sua nota p1? '))
+p2 = float(input('qual sua nota p2? '))
+if(p1>10 or p2>10):
+    print('Digite números menores ou iguais que 10')
+else:    
+    media = (p1+p2)/2
+    print('com as notas da p1 {} e da p2 {}\n a média é igual a {}'.format(p1, p2, media))
+    if(media>=6):
+        print('PARABÉNS VOCE FOI MUITO BEM')
+    else:
+        if(media >=5 and media <6):
+            print('QUASE MANO')
+        else: 
+            print('tu é um lixo mesmo')
+
+    
+
