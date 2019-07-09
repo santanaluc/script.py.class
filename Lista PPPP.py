6e3f3e4b2df6dd6be660bb272f9785e30367def8
Lucas Santana <47119652+santanaluc@users.noreply.github.com>
2019-07-09 11:47:27 -0300

    Add files via upload

---
+++ Lista PPPP.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 25 18:01:21 2019
+
+@author: lucas
+"""
+#Exercício 1 
+#lista = []
+#for i in range(4):
+ #   a = int(input('Digite uma lista: '))
+  #  lista.append(a)
+   # print(lista)
+    
+#Exercício 2
+def media():
+    soma = []
+    tamanho = []
+    media=soma/tamanho
+    tamanho.append(a)
+    soma.append(b)
+    return media
+
+#Programa Principal
+a = int(input('Digite o tamanho da média: '))
+b = int(input('Digite a soma dos valores: '))
+
+media()
